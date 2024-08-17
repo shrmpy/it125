4d84cc023c908c3888972763927db7e585ea8a46
shrmpy <>
2024-08-16 22:14:43 -0700

    sp for menu item, rev erd

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ Class project (hope to deploy via gh workflow/actions)
 ![ER](./foodtruck.svg)
 
 
-- background about 
-- example seed data 
-
+Share link to
+ [the Lucid Chart diagram](https://lucid.app/lucidchart/dc6640ea-9a83-47e5-9e5f-f7d0418432e4/edit?view_items=p2k1VE-~_3zx&invitationId=inv_cda4ec83-1c2a-4dde-80fe-af748356c846)
 
 ### Requirements
 
--- schema.sql
+++ schema.sql
@@ -71,8 +71,12 @@ ENGINE = InnoDB;
 CREATE TABLE `it125_foodtruck`.`orders` (
   `id`            INT          PRIMARY KEY AUTO_INCREMENT ,
   `created`       TIMESTAMP ,
+  `event_id`      INT       ,
   `patron_id`     INT       ,
   `till`          INT       ,
+  CONSTRAINT orders_fk_events
+    FOREIGN KEY (event_id)
+    REFERENCES events (id) ,
   CONSTRAINT orders_fk_patrons
     FOREIGN KEY (patron_id)
     REFERENCES patrons (id) ,
@@ -117,19 +121,97 @@ JOIN   menus mm ON ee.menu_id = mm.id
 ;
 CREATE OR REPLACE VIEW orders_view AS
 SELECT oo.id AS order_id, oi.quantity, oi.price ,
+       oo.created AS order_date ,
        mi.name AS menu_item ,
        pp.name AS patron_name ,
-       tt.name AS cashier_name
+       tt.name AS cashier_name ,
+       ll.name AS location_name ,
+       ll.longitude ,
+       ll.latitude 
 FROM   orders oo
 JOIN   order_item oi ON oo.id = oi.order_id
 JOIN   menu_item mi ON oi.menuitem_id = mi.id
+JOIN   events ee ON oo.event_id = ee.id
 JOIN   patrons pp ON oo.patron_id = pp.id
 JOIN   truckers tt ON oo.till = tt.id
+JOIN   locations ll ON ee.location_id = ll.id
 ;
 CREATE OR REPLACE VIEW patrons_view AS
 SELECT id, name, email, news_opt_in
 FROM   patrons
 ;
+
+DROP FUNCTION IF EXISTS last_menu_id;
+DELIMITER //
+CREATE FUNCTION last_menu_id()
+RETURNS INT
+DETERMINISTIC READS SQL DATA
+BEGIN
+  DECLARE seq_var INT;
+
+  -- find the last menu_id value
+  SELECT MAX(id) INTO seq_var FROM menus;
+
+  -- does MAX for empty table result in NULL?
+  -- if so, change the return val to zero.
+  IF seq_var IS NULL THEN
+    RETURN 0;
+  END IF;
+
+  RETURN(seq_var);
+END//
+DELIMITER ;
+
+-- sp to create/link menu items
+DROP PROCEDURE IF EXISTS insert_menuitem;
+DELIMITER //
+CREATE PROCEDURE insert_menuitem
+(
+  menu_id_param     INT ,
+  name_param        VARCHAR(50) ,
+  item_param        VARCHAR(50) ,
+  recipe_param      VARCHAR(255) ,
+  cost_param        DECIMAL(9, 2)
+)
+BEGIN
+  DECLARE sequence_pre INT;
+  DECLARE sequence_mid INT;
+
+  IF menu_id_param IS NOT NULL THEN
+      SET sequence_mid = menu_id_param;
+  ELSE
+    -- with null menu_id_param, action is to create new menu
+    SET sequence_pre = last_menu_id();
+    INSERT INTO menus (name) VALUES (name_param);
+
+    -- new menu should be sequence_pre + 1 
+    SET sequence_mid = last_menu_id();
+
+    IF sequence_mid <> (sequence_pre + 1) THEN
+      -- concurrent insert
+      SIGNAL SQLSTATE '22003'
+        SET MESSAGE_TEXT =
+          'Unexpected menu_id, another insert may be happening at the same time (need transaction).' ,
+        MYSQL_ERRNO = 1146;
+    END IF;
+
+  END IF;
+
+  -- link to new menu data
+  INSERT INTO menu_item
+  (name, recipe, cost, menu_id) 
+  VALUES 
+  (
+      item_param ,
+      recipe_param ,
+      cost_param ,
+      sequence_mid
+  );
+
+  SELECT * FROM menu_item WHERE menu_id = sequence_mid;
+END//
+DELIMITER ;
+
 
 DROP FUNCTION IF EXISTS last_order_id;
 DELIMITER //
@@ -141,7 +223,13 @@ BEGIN
 
   -- find the last order_id value
   SELECT MAX(id) INTO seq_var FROM orders;
- 
+
+  -- does MAX for empty table result in NULL?
+  -- if so, change the return val to zero.
+  IF seq_var IS NULL THEN
+    RETURN 0;
+  END IF;
+
   RETURN(seq_var);
 END//
 DELIMITER ;
@@ -152,6 +240,7 @@ DELIMITER //
 CREATE PROCEDURE insert_orderitem
 (
   order_id_param    INT ,
+  event_id_param    INT ,
   patron_id_param   INT ,
   till_param        INT ,
   quantity_param    INT ,
@@ -161,21 +250,28 @@ CREATE PROCEDURE insert_orderitem
 BEGIN
   DECLARE sequence_pre INT;
   DECLARE sequence_oid INT;
-  -- with null order_id_param, action is to create new order
-  IF order_id_param IS NULL THEN
+
+  IF order_id_param IS NOT NULL THEN
+      SET sequence_oid = order_id_param;
+  ELSE
+    -- with null order_id_param, action is to create new order
     SET sequence_pre = last_order_id();
-    INSERT INTO orders (created, patron_id, till) VALUES (SYSDATE(), patron_id_param, till_param);
+    INSERT INTO orders
+    (   created, event_id, patron_id, till )
+    VALUES
+    (   SYSDATE(), event_id_param, patron_id_param, till_param );
 
     -- new order should be sequence_pre + 1 
     SET sequence_oid = last_order_id();
+
     IF sequence_oid <> (sequence_pre + 1) THEN
+      -- concurrent insert
       SIGNAL SQLSTATE '22003'
         SET MESSAGE_TEXT =
           'Unexpected order_id, another insert may be happening at the same time (need transaction).' ,
         MYSQL_ERRNO = 1146;
     END IF;
-  ELSE
-    SET sequence_oid = order_id_param;
+
   END IF;
 
   -- link to new order data
@@ -212,19 +308,18 @@ INSERT INTO truckers (name, job, pay_rate, social_media) VALUES
 ('gordon', 'driver', 28, 'hellskitchen.tv') ,
 ('andres', 'grill', 25, 'wck.org');
 
-INSERT INTO menus (name) VALUES 
-('regular') ,
-('specials') ,
-('vegetarian') ,
-('farmers market') ,
-('holiday');
-
-INSERT INTO menu_item (name, recipe, cost, menu_id) VALUES 
-('taco', 'allrecipes.com/1', 1, 1) ,
-('chowder', 'allrecipes.com/2', 2, 2) ,
-('avocado toast', 'allrecipes.com/3', 3, 3) ,
-('grilled cheese', 'allrecipes.com/4', 4, 4) ,
-('pumpkin pie', 'allrecipes.com/5', 5, 5);
+-- use sp to create menus
+CALL insert_menuitem(NULL, 'regular', 'taco', 'allrecipes.com/1', 1);
+CALL insert_menuitem(NULL, 'specials', 'chowder', 'allrecipes.com/2', 2);
+CALL insert_menuitem(NULL, 'vegetarian', 'avocado toast', 'allrecipes.com/3', 3);
+CALL insert_menuitem(NULL, 'farmers market', 'grilled cheese', 'allrecipes.com/4', 4);
+CALL insert_menuitem(NULL, 'holiday', 'pumpkin pie', 'allrecipes.com/5', 5);
+-- use sp to extra menu items
+CALL insert_menuitem(1, 'regular', 'nachos', 'allrecipes.com/11', 0.99);
+CALL insert_menuitem(2, 'specials', 'chili', 'allrecipes.com/22', 1.99);
+CALL insert_menuitem(3, 'vegetarian', 'corn on the cob', 'allrecipes.com/33', 2.99);
+CALL insert_menuitem(4, 'farmers market', 'mac and cheese', 'allrecipes.com/44', 3.99);
+CALL insert_menuitem(5, 'holiday', 'cranberry sauce', 'allrecipes.com/55', 4.99);
 
 INSERT INTO locations (name, longitude, latitude) VALUES 
 ('university village', '47.6425777', '-122.3527938') ,
@@ -240,21 +335,22 @@ INSERT INTO events (name, promo_url, location_id, repeating, start, end, menu_id
 ('market', 'meetup.com/4', 4, 'weekly', NOW(), DATE_ADD(NOW(), INTERVAL 3 MONTH), 4) ,
 ('fair', 'meetup.com/5', 5, 'annual', NOW(), DATE_ADD(NOW(), INTERVAL 3 MONTH), 5);
 
+
 -- use sp to create orders
-CALL insert_orderitem(NULL, 1, 1, 1, 1, 1);
-CALL insert_orderitem(NULL, 2, 2, 2, 2, 2);
-CALL insert_orderitem(NULL, 3, 3, 3, 3, 3);
-CALL insert_orderitem(NULL, 4, 4, 4, 4, 4);
-CALL insert_orderitem(NULL, 5, 5, 5, 5, 5);
+CALL insert_orderitem(NULL, 1, 1, 1, 1, 1, 1);
+CALL insert_orderitem(NULL, 2, 2, 2, 2, 2, 2);
+CALL insert_orderitem(NULL, 3, 3, 3, 3, 3, 3);
+CALL insert_orderitem(NULL, 4, 4, 4, 4, 4, 4);
+CALL insert_orderitem(NULL, 5, 5, 5, 5, 5, 5);
 
 -- use sp to extra order line items
-CALL insert_orderitem(1, 1, 1, 2, 1.99, 2);
-CALL insert_orderitem(2, 2, 2, 3, 2.99, 3);
-CALL insert_orderitem(3, 3, 3, 4, 3.99, 4);
-CALL insert_orderitem(3, 3, 3, 5, 4.99, 5);
-CALL insert_orderitem(3, 3, 3, 1, 0.99, 1);
-CALL insert_orderitem(4, 4, 4, 5, 4.99, 5);
-CALL insert_orderitem(5, 5, 5, 1, 0.99, 1);
+CALL insert_orderitem(1, 1, 1, 1, 2, 1.99, 2);
+CALL insert_orderitem(2, 2, 2, 2, 3, 2.99, 3);
+CALL insert_orderitem(3, 3, 3, 3, 4, 3.99, 4);
+CALL insert_orderitem(3, 3, 3, 3, 5, 4.99, 5);
+CALL insert_orderitem(3, 3, 3, 3, 1, 0.99, 1);
+CALL insert_orderitem(4, 4, 4, 4, 5, 4.99, 5);
+CALL insert_orderitem(5, 5, 5, 5, 1, 0.99, 1);
 
 -- 
 -- it125 project (food truck queries)
@@ -282,9 +378,17 @@ FROM     patrons_view
 WHERE    news_opt_in = TRUE
 ;
 
--- look up most sales
+-- look up highest sales
 SELECT   cashier_name, COUNT(cashier_name) AS items_count
 FROM     orders_view
 GROUP BY 1
 ORDER BY 2 DESC
 ;
+
+-- look up most popular location
+SELECT   location_name, longitude, latitude, COUNT(location_name) AS items_count
+FROM     orders_view
+GROUP BY 1, 2, 3
+ORDER BY 3 DESC
+;
+
